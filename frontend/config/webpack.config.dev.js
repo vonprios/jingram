d763fb82449e0882deef78b94c44209407f69efe
jin <vonprios@gmail.com>
2018-05-31 18:13:11 +0900

    camelCase : "dashes" listItem(list-item) Adding scss variables data: `@import "${paths.appSrc}/config/_variables.scss";`

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -166,6 +166,7 @@ module.exports = {
                   importLoaders: 1,
                   modules: true,
                   localIdentName: "[path][name]__[local]--[hash:base64:5]",
+                  camelCase: "dashes",
                 },
               },
               {
@@ -192,7 +193,8 @@ module.exports = {
               {
                 loader: require.resolve('sass-loader'),
                 options:{
-                  sourceMap: true
+                  sourceMap: true,
+                  data: `@import "${paths.appSrc}/config/_variables.scss";`
                 }
               }
             ],

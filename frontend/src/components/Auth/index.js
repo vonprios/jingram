426f9cd3d5f4ae4a9010a42ee9700d3c3ed96dbb
jin <vonprios@gmail.com>
2018-06-01 20:51:15 +0900

    #3-24 Auth Component part One

---
+++ frontend/src/components/Auth/index.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import Container from "./container";
+
+// Add all the actions for 이곳에 추가할것
+// Log in 로그인
+// Sign up 회원가입
+// Recover password 비밀번호 찾기
+// Check username 유저명 확인(중복확인)
+// Check password 비밀번호 확인(강력한 비번인지)
+// Check email 이메일 확인
+
+export default connect()(Container);

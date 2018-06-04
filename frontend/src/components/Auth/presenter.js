dd62b4400f61a8c3da3832ca90f2e22c9b483a6d
jin <vonprios@gmail.com>
2018-06-04 19:26:06 +0900

    #3-26 Auth Component part Three

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./styles.scss";
+import { LoginForm, SignupForm } from "components/AuthForms";
 
 const Auth = (props, context) => (
   <main className={styles.auth}>
@@ -7,37 +8,27 @@ const Auth = (props, context) => (
       <img src={require("images/phone.png")} alt="Checkour our app. Is cool" />
     </div>
     <div className={styles.column}>
+      <div className={`${styles.whiteBox} ${styles.formBox}`}>
+        {props.action == "login" && <LoginForm />}
+        {props.action == "signup" && <SignupForm />}
+      </div>
       <div className={styles.whiteBox}>
-        {(() => {
-          switch (props.action) {
-            case "login":
-              return (
-                <p>
-                  계정이 없으신가요?{" "}
-                  <span
-                    onClick={props.changeAction}
-                    className={styles.changeLink}
-                  >
-                    가입하기
-                  </span>
-                </p>
-              );
-            case "signup":
-              return (
-                <p>
-                  계정이 있으신가요?{" "}
-                  <span
-                    onClick={props.changeAction}                  
-                    className={styles.changeLink}
-                  >
-                    로그인
-                  </span>
-                </p>
-              );
-            default:
-              return null;
-          }
-        })()}
+        {props.action === "login" && (
+          <p>
+            Don't have an account?{" "}
+            <span onClick={props.changeAction} className={styles.changeLink}>
+              Sign up
+            </span>
+          </p>
+        )}
+        {props.action === "signup" && (
+          <p>
+            Have an account?{" "}
+            <span onClick={props.changeAction} className={styles.changeLink}>
+              Log in
+            </span>
+          </p>
+        )}
       </div>
       <div className={styles.appBox}>
         <span>Get the app</span>

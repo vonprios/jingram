a72e9b78e55f651de7841488a4a7c3baafdc67ad
jin <vonprios@gmail.com>
2018-06-05 17:45:15 +0900

    #3-32 Importing Translations

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,23 +1,25 @@
 export const translations = {
-  kr: {
-    "About Us": "Instagram 정보",
-    Support: "지원",
-    Blog: "블로그",
-    Press: "홍보 센터",
-    API: "API",
-    Jobs: "채용 정보",
-    Privacy: "개인정보처리방침",
-    Terms: "약관",
-    Directory: "디렉터리",
-    Language: "언어",
-    Username: "전화번호, 사용자 이름 또는 이메일",
-    Password: "비밀번호",
-    Login: "로그인",
-    or: "또는",
-    "Log in with Facebook": "Facebook으로 계속하기",
-    "Forgot password?": "비밀번호를 잊으셨나요?",
-    "Don't have an account?": "계정이 없으신가요?",
-    "Sign up": "가입하기",
-    "Get the app": "앱을 다운로드하세요."
-  }
-};
+  'kr': {
+    'Don\'t have an account?': '계정이 없으신가요?',
+    'Sign up': '가입하기',
+    'Get the app': '앱을 다운로드하세요.',
+    'Username': '',
+    'Password': '',
+    'Login': '',
+    'or': '',
+    'Log in with Facebook': '',
+    'Forgot password?': '',
+    'About Us': '',
+    'Support': '',
+    'Blog': '',
+    'Press': '',
+    'API': '',
+    'Jobs': '',
+    'Privacy': '',
+    'Terms': '',
+    'Directory': '',
+    'Language': '',
+  },
+  'options': {
+  },
+}
